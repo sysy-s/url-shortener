c255b6769d1e0fcc56d18a6bec178cc57976411b
sysy-s <szymon.bold@gmail.com>
2022-03-08 20:48:39 +0100

    changed stat ordering got better UX

--- app/routers/stats.py
+++ app/routers/stats.py
@@ -21,7 +21,8 @@ def get_user_url_statistics(search: Optional[str] = "", limit: int = 25,
     current_user_urls = db.query(models.Url).filter(
                         and_(models.Url.user_id==current_user.id,
                         or_(models.Url.short.contains(search),
-                            models.Url.long.contains(search)))).limit(limit).all()
+                            models.Url.long.contains(search)))).order_by(models.Url.last_clicked.desc().nullslast()).limit(limit).all()
+
     return current_user_urls
 
 @router.get("/{short}")
@@ -32,17 +33,15 @@ def get_specific_url_statistics(response: Response, short: str,
     url = db.query(models.Url).filter(models.Url.short==short).first()
     
     if url is None:
-        return HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Url not found")
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Url not found")
         
     if url.user_id is not current_user.id:
-        return HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="You're not the owner of this url")
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="You're not the owner of this url")
     
     all_visits = db.query(models.Visit).filter(models.Visit.url_id==url.id).all()
-    if all_visits:
-        response.model = List[schemas.Visit]
-        return all_visits
+    response.model = List[schemas.Visit]
+    return all_visits
 
-    return {"wow": "such empty"}
 
 @router.put("/{short}")
 def update_url(short: str,
@@ -61,13 +60,13 @@ def update_url(short: str,
     """
 
     if url is None:
-        return HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Url not found")
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Url not found")
 
     if url.user_id is not current_user.id:
-        return HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="You're not the owner of this url")
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="You're not the owner of this url")
 
     if not validators.url(updated_url_data.long) or not updated_url_data.short.isalnum():
-        return HTTPException(status_code=status.HTTP_409_CONFLICT, detail=f"Url '{updated_url_data.long}' or '{BASE_URL}/{updated_url_data.short}' is not a valid url")
+        raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail=f"Url '{updated_url_data.long}' or '{BASE_URL}/{updated_url_data.short}' is not a valid url")
 
     url_query.update(updated_url_data.dict())
     db.commit()
@@ -87,10 +86,10 @@ def delete_url_visits(short: str,
     url = url_query.first()
 
     if url is None:
-        return HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Url not found")
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Url not found")
 
     if url.user_id is not current_user.id:
-        return HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="You're not the owner of this url")
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="You're not the owner of this url")
 
     visits_query = db.query(models.Visit).filter(models.Visit.url_id==url.id)
     visits_query.delete(synchronize_session=False)
@@ -111,7 +110,7 @@ def delete_url(url_to_delete: schemas.UrlDelete,
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Url {url_to_delete.short} was not found")
     
     if url.user_id is not current_user.id:
-        return HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="You're not the owner of this url")
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="You're not the owner of this url")
     
     url_query.delete(synchronize_session=False)
     db.commit()
